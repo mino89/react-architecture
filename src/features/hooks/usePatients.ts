62cd110afd486f2dcd634097f8003015c8d62475
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2024-09-28 18:40:07 +0200

    refactor: removed useless data

--- src/features/hooks/usePatients.ts
+++ src/features/hooks/usePatients.ts
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useService } from "../../core/hooks/useService";
 import { PatientService } from "../services/patient/patient-service";
-import { ColumnConfig } from "../../components/Table/_types";
 import { useModal } from "../../components/Modal/useModal";
 import { PatientListItem } from "../services/patient/_types";
 
@@ -18,45 +17,6 @@ export function usePatients() {
   }, [service]);
   
 
-  const columnsConfig: ColumnConfig[] = [
-    {
-      key: "familyName",
-      label: "Family Name",
-      filterType: "text",
-      type: "string",
-    },
-    {
-      key: "givenName",
-      label: "Given Name",
-      filterType: "text",
-      type: "string",
-    },
-    {
-      key: "birthDate",
-      label: "Birth Date",
-      filterType: "select",
-      type: "date",
-    },
-    {
-      key: "sex",
-      label: "Sex",
-      filterType: "select",
-      type: "string",
-    },
-    {
-      key: "numberOfParameters",
-      label: "Number of Parameters",
-      filterType: "text",
-      type: "number",
-    },
-    {
-      key: "hasAlarm",
-      label: "Has Alarm",
-      filterType: "select",
-      type: "boolean",
-    },
-  ];
-
   const openDetail = (row: PatientListItem) => {
     setSelectedPatient(row.id)
     openModal();
@@ -64,7 +24,6 @@ export function usePatients() {
 
   return {
     patients,
-    columnsConfig,
     isOpen,
     closeModal,
     openDetail,
