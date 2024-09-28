a04dfd0b4d543656f4c3900e161513f98ab7bd78
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2024-09-28 15:42:50 +0200

    docs: di and utils

--- src/core/hooks/useService.ts
+++ src/core/hooks/useService.ts
@@ -1,5 +1,10 @@
 import { diContainer } from "../di/container";
 
+/**
+ * 
+ * @param id the id of service in the DI container 
+ * @returns the service instance
+ */
 export function useService<T>(id: string): T {
   return diContainer.get<T>(id);
 }
