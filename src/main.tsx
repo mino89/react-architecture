b8e78280affbfe20dc5088ab2d52f31e9a65913d
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2024-10-02 11:50:14 +0200

    chore: removed log

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,5 @@ import "reflect-metadata";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-console.log(import.meta.env);
+
 createRoot(document.getElementById("root")!).render(<App />);
