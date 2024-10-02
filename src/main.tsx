639978f6003d1b85a76e0497b3c9a34ad94d9e3b
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2024-10-02 11:43:11 +0200

    test: verify if env is

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import "reflect-metadata";
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+console.log(import.meta.env);
+createRoot(document.getElementById("root")!).render(<App />);
