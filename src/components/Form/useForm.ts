b93b022c20c2d02048fbf1f7690f7ecec920da05
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2024-10-07 01:15:10 +0200

    fix: error on build

--- src/components/Form/useForm.ts
+++ src/components/Form/useForm.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 
 export const useForm = (data: any, onSubmit: Function) => {
   const [formData, setFormData] = useState<any>(data);
